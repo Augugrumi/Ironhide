7e2ec8c05c5edf11f436b2fe9318d3f7c4bea8a2
mzanella <zanna0150@gmail.com>
2018-10-10 10:20:38 +0200

    Poor implementation of a classifier

--- src/classifier/classifier.cpp
+++ src/classifier/classifier.cpp
@@ -2,6 +2,8 @@
 // Created by zanna on 05/10/18.
 //
 
+#include <utils/ipheader.h>
+#include <utils/packetutils.h>
 #include "classifier.h"
 
 char* classifier::Classifier::classify_pkt(unsigned char *pkt,
@@ -16,6 +18,26 @@ void classifier::Classifier::Mapper::set_pkt(unsigned char *pkt,
     //TODO take the packet, check the header and at least classify based on
     //     protocol. At the end set the type_ to not save all the pkt
     //     do not know how to do? default chain
+    if (pkt_len > sizeof(utils::ip_header_t)) {
+        utils::ip_header_t h_ip = utils::PacketUtils::retrieve_ip_header(pkt);
+        switch (h_ip.protocol) {
+            /* refers to https://en.wikipedia.org/wiki/List_of_IP_protocol_numbers
+             * for a complete list if other protocols are needed e.g.
+             * case 1:
+             *   type_ = icmp packets;
+             *   break;
+             */
+            case 6:
+                type_ = classifier::TCP;
+                break;
+            case 17:
+                type_ = classifier::UDP;
+                break;
+            default:
+                type_ = classifier::DEFAULT;
+                break;
+        }
+    }
     type_ = classifier::DEFAULT;
 }
 
