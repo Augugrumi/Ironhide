906302fa62ac6e1b8faaf0eeffc1a0a64d1702c4
mzanella <zanna0150@gmail.com>
2018-10-16 12:30:54 +0200

    Add protocol to connection entry

--- src/endpoint/connectionentry.cpp
+++ src/endpoint/connectionentry.cpp
@@ -8,13 +8,16 @@ endpoint::ConnectionEntry::ConnectionEntry(const std::string &ip_src,
                                            const std::string &ip_dst,
                                            const uint16_t & port_src,
                                            const uint16_t & port_dst,
-                                           const std::string & sfcid)
+                                           const std::string & sfcid,
+                                           const db::protocol_type & prt)
         : ip_src_(ip_src), ip_dst_(ip_dst),
-          port_src_(port_src), port_dst_(port_dst), sfcid_(sfcid){}
+        port_src_(port_src), port_dst_(port_dst),
+        sfcid_(sfcid), prt_(prt){}
 
 bool endpoint::ConnectionEntry::operator==(
         const endpoint::ConnectionEntry &ce) const {
-    return ip_src_ == ce.ip_src_ &&
+    return prt_ == ce.prt_ &&
+           ip_src_ == ce.ip_src_ &&
            ip_dst_ == ce.ip_dst_ &&
            port_src_ == ce.port_src_ &&
            port_dst_ == ce.port_dst_ &&
@@ -23,7 +26,13 @@ bool endpoint::ConnectionEntry::operator==(
 
 bool endpoint::ConnectionEntry::operator<(
         const endpoint::ConnectionEntry &ce) const {
-    return sfcid_ < ce.sfcid_;
+    // workaround to use with maps, it does mean nothing operator < for this class
+    return prt_ < ce.prt_ ||
+           ip_src_ < ce.ip_src_ ||
+           ip_dst_ < ce.ip_dst_ ||
+           port_src_ < ce.port_src_ ||
+           port_dst_ < ce.port_dst_ ||
+           sfcid_ < ce.sfcid_;
 }
 
 const std::string &endpoint::ConnectionEntry::get_ip_src() const {
@@ -44,4 +53,8 @@ uint16_t endpoint::ConnectionEntry::get_port_dst() const {
 
 const std::string &endpoint::ConnectionEntry::get_sfcid() const {
     return sfcid_;
+}
+
+const db::protocol_type endpoint::ConnectionEntry::get_protocol_type() const {
+    return prt_;
 }
--- src/endpoint/connectionentry.h
+++ src/endpoint/connectionentry.h
@@ -6,6 +6,7 @@
 #define IRONHIDE_CONNECTIONENTRY_H
 
 #include <string>
+#include "dbquery.h"
 
 namespace endpoint {
 
@@ -16,12 +17,14 @@ private:
     const uint16_t port_src_;
     const uint16_t port_dst_;
     const std::string sfcid_;
+    const db::protocol_type prt_;
 public:
     ConnectionEntry(const std::string& ip_src,
                     const std::string& ip_dst,
                     const uint16_t & port_src,
                     const uint16_t & port_dst,
-                    const std::string & sfcid);
+                    const std::string & sfcid,
+                    const db::protocol_type& prt);
     bool operator==(const ConnectionEntry& ce) const;
     bool operator<(const ConnectionEntry& ce) const;
     const std::string &get_ip_src() const;
@@ -29,6 +32,7 @@ public:
     uint16_t get_port_src() const;
     uint16_t get_port_dst() const;
     const std::string &get_sfcid() const;
+    const db::protocol_type get_protocol_type() const;
 };
 
 }
