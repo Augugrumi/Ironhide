67e6297ba84c276f675f2b84bec2a88803b07ebc
mzanella <zanna0150@gmail.com>
2018-10-18 18:11:00 +0200

    Remove useless definition of sfc header length

--- src/endpoint/utils/sfcheaderstruct.h
+++ src/endpoint/utils/sfcheaderstruct.h
@@ -36,5 +36,4 @@ struct sfc_header {
     uint32_t other:24;
 }__attribute__((__packed__)); // disallow optimization of byte positioning
 
-const unsigned short int SFCHDR_LEN = sizeof(sfc_header);
 #endif //IRONHIDE_SFCHEADERSTRUCT_H
